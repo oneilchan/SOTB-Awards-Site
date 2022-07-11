5718571fac76e4178321777ebd2d3ea3cd6980a4
oneilchan <dariesisaiah@gmail.com>
2022-07-11 16:52:11 +0200

    fixed admin site

--- midyearsotbawards/settings.py
+++ midyearsotbawards/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['sotbawards.azurewebsites.net', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS=['https://sotbawards.azurewebsites.net']
 # Application definition
 
 INSTALLED_APPS = [
